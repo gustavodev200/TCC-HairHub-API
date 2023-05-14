a55278c50b9092e5043cfa3677569eea4e164907
Gustavo Lage <gustavolage200@gmail.com>
2023-05-14 00:32:37 -0300

    feat: change status category done

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,9 +7,9 @@ import { categoryRoutes } from "./category.routes";
 
 const routes = Router();
 
-routes.use("/services", authenticationHandler, serviceRoutes);
 routes.use("/employees", authenticationHandler, employeeRoutes);
 routes.use("/categories", authenticationHandler, categoryRoutes);
+routes.use("/services", authenticationHandler, serviceRoutes);
 routes.use("/auth", authRoutes);
 
 export { routes };
